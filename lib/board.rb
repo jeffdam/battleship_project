85addb1952851ab6542e068f65438d3045513659
Jeff Dam <jeff.dam@gmail.com>
2019-03-23 16:59:02 -0700

    Add Board#[] and Board #[]=

--- lib/board.rb
+++ lib/board.rb
@@ -7,4 +7,14 @@ class Board
         @size = num ** 2
     end
 
+    def [](pos)
+        a,b = pos
+        @grid[a][b]
+    end
+
+    def []=(pos, val)
+        a,b = pos
+        @grid[a][b] = val
+    end
+
 end
