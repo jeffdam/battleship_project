a668576c36fdbdbf5eced1dd7662b0e1f92feb96
Jeff Dam <jeff.dam@gmail.com>
2019-03-23 17:50:01 -0700

    Add Battleship#initialize, Battleship#start_game, Battleship#lose?, Battleship#win?, Battleship#game_over?, and  Battleship#turn

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -3,4 +3,47 @@ require_relative "player"
 
 class Battleship
 
+  attr_reader :board, :player
+
+  def initialize(n)
+    @player = Player.new
+    @board = Board.new(n)
+    @remaining_misses = @board.size / 2
+  end
+
+  def start_game
+    @board.place_random_ships
+    p @board.num_ships
+    @board.print
+  end 
+
+  def lose?
+    if @remaining_misses <= 0
+      p "you lose"
+      return true
+    end
+    false
+  end
+
+  def win?
+    if @board.num_ships == 0
+      p "you win"
+      return true
+    end
+    false
+  end
+
+  def game_over?
+    return true if lose? || win?
+    false
+  end
+
+  def turn
+    unless @board.attack(@player.get_move)
+      @remaining_misses -= 1
+    end
+    @board.print
+    p @remaining_misses
+  end
+
 end
